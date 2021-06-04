3b0d541e8ae5885674f9d8188ac92a6862425a7b
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2021-06-04 18:08:56 +0200

    -

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 (async () => {
 
-  prompts = require('prompts'), { login } = require('./functions/login'), { register } = require('./functions/register'), { settings} = require('./functions/settings')   
+  prompts = require('prompts'), { login } = require('./functions/login'), { register } = require('./functions/register'), { settings } = require('./functions/settings')
 
-    const menu = await prompts([
-        {
-            type: 'select',
-            name: 'value',
-            message: 'What do you want to do?',
-            choices: [
-              { title: 'Register', value: 'register' },
-              { title: 'Login', value: 'login'},
-              { title: 'Settings', value: 'settings' }
-            ],
-            initial: 1
-          }
-    ]);
-    eval(menu.value)()
-  })();
+  const menu = await prompts([{ type: 'select', name: 'value', message: 'What do you want to do?', choices: [{ title: 'Register', value: 'register' }, { title: 'Login', value: 'login' }, { title: 'Settings', value: 'settings' }], initial: 1 }]);
+
+  
+  eval(menu.value)()
+})();
