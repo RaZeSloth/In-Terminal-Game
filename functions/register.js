7f376d02d6cada99dcacedc2cf791b28756a5f50
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2021-07-02 16:09:59 +0200

    Finished register

--- functions/register.js
+++ functions/register.js
@@ -3,57 +3,89 @@ module.exports.register = function register() {
 
     chalk = require('chalk'), prompts = require('prompts'), { connection } = require('../database/db')
 
-    let username = await prompts([{ type: 'text', name: 'value', message: `Create your username:` }]);
-
-
-    if (username.value.length > 20) {
-
-      do {
-
-        console.log(chalk.red('\nSomething is wrong with your username!\nUsername Requirements: MUST be shorter than 8 characters\n'))
-
-        var username2 = await prompts([{ type: 'password', name: 'value', message: `Create your username:` }]);
-
-        if (!username2.value.length > 20) {
-          break;
-        }
-      }
-      while (username2.value.length > 20);
-
-    }
-
+    var username = await prompts([{ type: 'text', name: 'value', message: `Create your username:` }]);
+    console.clear()
     
+    if (username.value.length > 20 || username.value.length <= 2) {
 
-    let password = await prompts([{ type: 'password', name: 'value', message: `Create your password:` }]);
+        do {
 
-    if (password.value.length > 20 || password.value.length < 8) {
+          console.log(chalk.red('\nSomething is wrong with your username!\nUsername Requirements: MUST be longer than 2 characters, MUST be shorter than 8 characters\n'))
 
-      do {
+          var username = await prompts([{ type: 'text', name: 'value', message: `Create your username:` }]);
+          console.clear()
 
-        console.log(chalk.red('\nSomething is wrong with your password!\nPassword Requirements: MUST be longer than 8 characters, MUST be shorter than 8 characters\n'))
-
-        var password2 = await prompts([{ type: 'password', name: 'value', message: `Create your password:` }]);
-
-        if (!password2.value.length > 20 && !password2.value.length < 8) {
-          break;
+          if (!username.value.length > 20 && !username.value.length <= 2) {
+            break;
+          }
         }
+        while (username.value.length > 20 || username.value.length <= 2);
+
       }
-      while (password2.value.length > 20 || password2.value.length < 8);
-
-    }
 
 
-    let job = await prompts([{ type: 'select', name: 'value', message: `Okay ${username.value}, what do you want to do as job?`, choices: [{ title: 'MacDonalds Manager', value: 'mcmanager' }, { title: 'Web Developer', value: 'webdev' }, { title: 'Professional Gamer', value: 'profgamer' }, { title: 'Youtuber', value: 'youtuber' }, { title: 'Designer', value: 'designer' },], initial: 1 }]);
 
-    if (password.value.length > 20 || password.value.length < 8 && username.value.length > 20) {
-      connection.query(`INSERT INTO Accounts VALUES ('${username2.value}', '${password2.value}', '${job.value}', '0')`)
-    } else if (!password.value.length > 20 && !password.value.length < 8 && username.value.length > 20) {
-      connection.query(`INSERT INTO Accounts VALUES ('${username2.value}', '${password.value}', '${job.value}', '0')`)
-    } else if (password.value.length > 20 || password.value.length < 8 && !username.value.length > 20) {
-      connection.query(`INSERT INTO Accounts VALUES ('${username.value}', '${password2.value}', '${job.value}', '0')`)
-    } else {
-      connection.query(`INSERT INTO Accounts VALUES ('${username.value}', '${password.value}', '${job.value}', '0')`)
-    }
+      var password = await prompts([{ type: 'password', name: 'value', message: `Create your password:` }]);
+      console.clear()
+
+      if (password.value.length > 20 || password.value.length < 8) {
+
+        do {
+
+          console.log(chalk.red('\nSomething is wrong with your password!\nPassword Requirements: MUST be longer than 8 characters, MUST be shorter than 8 characters\n'))
+
+          var password = await prompts([{ type: 'password', name: 'value', message: `Create your password:` }]);
+          console.clear()
+
+          if (!password.value.length > 20 && !password.value.length < 8) {
+            break;
+          }
+        }
+        while (password.value.length > 20 || password.value.length < 8);
+
+      }
+
+      var confirmpassword = await prompts([{ type: 'password', name: 'value', message: `Confirm your password:` }]);
+      console.clear()
+
+      if (confirmpassword.value != password.value) {
+
+        do {
+
+          console.log(chalk.red('\nThe 2 passwords do not match!\n'))
+
+          var confirmpassword = await prompts([{ type: 'password', name: 'value', message: `Confirm your password:` }]);
+          console.clear()
+
+          if (confirmpassword.value == password.value) {
+            break;
+          }
+        }
+        while (confirmpassword.value != password.value);
+
+      }
+
+
+      let job = await prompts([{ type: 'select', name: 'value', message: `Okay ${username.value}, what do you want to do as job?`, choices: [{ title: 'MacDonalds Manager', value: 'mcmanager' }, { title: 'Web Developer', value: 'webdev' }, { title: 'Professional Gamer', value: 'profgamer' }, { title: 'Youtuber', value: 'youtuber' }, { title: 'Designer', value: 'designer' },], initial: 1 }]);
+      console.clear()
+      
+      
+      if (username.value && password.value && job.value) {
+      
+      connection.query(`INSERT INTO Accounts VALUES ('${username.value}', '${password.value}', '${job.value}', '0')`, function(err) {
+        if (err) {
+          return console.log(chalk.red('\nSomething went wrong while processing the data to the database!\nPossible causes:\n- You have already created an account with this username.\n- You do not have a MySQL server on your PC.\n- You have not modified the data in the .env file as requested.\n'))
+        }
+      })
+      await console.log(chalk.green('Your account has been successfully created!'))
+
+      } else {
+        return console.log(chalk.red('\nYour account could not be created because not all data has been entered.\n'))
+      }
+
+
+
+
 
   })();
 }
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 (async () => {
 
+  console.clear()
+
   prompts = require('prompts'), { login } = require('./functions/login'), { register } = require('./functions/register'), { settings } = require('./functions/settings')
 
   const menu = await prompts([{ type: 'select', name: 'value', message: 'What do you want to do?', choices: [{ title: 'Register', value: 'register' }, { title: 'Login', value: 'login' }, { title: 'Settings', value: 'settings' }], initial: 1 }]);
-
+  console.clear()
   
   eval(menu.value)()
 })();
