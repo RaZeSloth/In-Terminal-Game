8b1cfc188fcdc10305408550cbae71daf7ee3437
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2021-07-04 10:00:24 +0200

    Finished index, (auto login soon)

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
   console.clear()
 
-  prompts = require('prompts'), { login } = require('./functions/login'), { register } = require('./functions/register'), { settings } = require('./functions/settings')
+  prompts = require('prompts'), { login } = require('./functions/login'), { register } = require('./functions/register'), { settings } = require('./functions/settings'), { info } = require('./functions/info')
 
-  const menu = await prompts([{ type: 'select', name: 'value', message: 'What do you want to do?', choices: [{ title: 'Register', value: 'register' }, { title: 'Login', value: 'login' }, { title: 'Settings', value: 'settings' }], initial: 1 }]);
+  const menu = await prompts([{ type: 'select', name: 'value', message: 'What do you want to do?', choices: [{ title: 'Register', value: 'register' }, { title: 'Login', value: 'login' }, { title: 'Settings', value: 'settings' }, { title: 'Info', value: 'info'}], initial: 1 }]);
   console.clear()
   
   eval(menu.value)()
