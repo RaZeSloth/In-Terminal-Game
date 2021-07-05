b77fb847a723acf6f58f914f85e10f0d51c7ca73
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2021-07-05 13:28:56 +0200

    Made it better!

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
   console.clear()
 
-  prompts = require('prompts'), { login } = require('./functions/login'), { register } = require('./functions/register'), { settings } = require('./functions/settings'), { info } = require('./functions/info')
+  chalk = require('chalk'), prompts = require('prompts'), { login } = require('./functions/login'), { register } = require('./functions/register'), { settings } = require('./functions/settings'), { info } = require('./functions/info')
 
-  const menu = await prompts([{ type: 'select', name: 'value', message: 'What do you want to do?', choices: [{ title: 'Register', value: 'register' }, { title: 'Login', value: 'login' }, { title: 'Settings', value: 'settings' }, { title: 'Info', value: 'info'}], initial: 1 }]);
+  console.log(chalk.gray('- Menu\n'))
+  let menu = await prompts([{ type: 'select', name: 'value', message: 'What do you want to do?', choices: [{ title: 'Register', value: 'register' }, { title: 'Login', value: 'login' }, { title: 'Settings', value: 'settings' }, { title: 'Info', value: 'info' }], initial: 1 }]);
   console.clear()
-  
+
   eval(menu.value)()
 })();
