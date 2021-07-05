21eecc3eb721efc6ed48dc5939ef8b7fa7c00c74
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2021-07-05 13:25:30 +0200

    Fixed balance data-type

--- database/schema.sql
+++ database/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE Accounts (
     username VARCHAR(20) NOT NULL PRIMARY KEY,
     pass VARCHAR(20),
     job VARCHAR(20),
-    balance VARCHAR(20),
+    balance INT,
     active BOOLEAN
 );
