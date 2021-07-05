8799cf8e6ab2437e08a90e7b0178c8cbf9ae9d31
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2021-07-05 13:26:32 +0200

    Finished logout.js

--- functions/logout.js
+++ functions/logout.js
@@ -1,3 +1,44 @@
 module.exports.logout = function logout() {
-    console.log(chalk.red('Not implemented yet'))
+    (async () => {
+
+        chalk = require('chalk'), prompts = require('prompts'), { connection } = require('../database/db'), { menu } = require('../functions/menu'), { settings } = require('../functions/settings'), { work } = require('../functions/work')
+
+        let sure = await prompts([{ type: 'toggle', name: 'value', message: 'Are u sure you want to log out?', initial: true, active: 'yes', inactive: 'no' }])
+
+        if (sure.value) {
+
+            connection.query(`SELECT username FROM Accounts WHERE active = '1'`, async (err, activeaccount) => {
+                let activeaccountusername = activeaccount[0].username
+
+                connection.query(`UPDATE Accounts SET active = '0' WHERE username = '${activeaccountusername}'`)
+                console.clear()
+                console.log(chalk.green('Successfully logged out!'))
+                console.log('↻ | Loading…')
+                setTimeout(function () {
+                    console.clear()
+                    console.log(chalk.green('Successfully logged out!'))
+                    console.log('↷ | Loading…')
+
+                    setTimeout(function () {
+                        console.clear()
+                        console.log(chalk.green('Successfully logged out!'))
+                        console.log('↻ | Loading…')
+
+                        setTimeout(function () {
+                            menu()
+                        }, 500)
+                    }, 500)
+                }, 500)
+            })
+
+        } else {
+            console.clear()
+            let choosemenu = await prompts([{ type: 'select', name: 'value', message: 'What do you want to do now?', choices: [{ title: 'Log-Out', value: 'logout' }, { title: 'Settings', value: 'settings' }, { title: 'Go to work', value: 'work' }], initial: 1 }]);
+            console.clear()
+
+            eval(choosemenu.value)()
+        }
+
+
+    })();
 }
