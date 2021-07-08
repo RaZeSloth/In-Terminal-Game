bd5a6cdf56b884d528e57ff6bdcf211f856dfd7a
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2021-07-08 08:09:19 +0200

    Finished login.js

--- functions/login.js
+++ functions/login.js
@@ -12,23 +12,10 @@ module.exports.login = function login() {
             if (!account[0]) {
 
                 console.clear()
-                console.log(chalk.red('\nThere is no known account with this username!\n'))
-                console.log('↻ | Loading…')
-                setTimeout(function () {
-                    console.clear()
-                    console.log(chalk.red('\nThere is no known account with this username!\n'))
-                    console.log('↷ | Loading…')
-
-                    setTimeout(function () {
-                        console.clear()
-                        console.log(chalk.red('\nThere is no known account with this username!\n'))
-                        console.log('↻ | Loading…')
-
-                        setTimeout(function () {
-                            return menu()
-                        }, 500)
-                    }, 500)
-                }, 500)
+                console.log(chalk.red('There is no known account with this username!\n'))
+                let pressenter = await prompts([{ type: 'text', name: 'value', message: 'Press enter to go back to the menu ...'}])
+                console.clear()
+                menu()
 
 
             } else {
@@ -42,50 +29,23 @@ module.exports.login = function login() {
                 if (account[0].pass != password.value) {
 
                     console.clear()
-                    console.log(chalk.red('\nWrong password entered!\n'))
-                    console.log('↻ | Loading…')
-                    setTimeout(function () {
-                        console.clear()
-                        console.log(chalk.red('\nWrong password entered!\n'))
-                        console.log('↷ | Loading…')
-    
-                        setTimeout(function () {
-                            console.clear()
-                            console.log(chalk.red('\nWrong password entered!\n'))
-                            console.log('↻ | Loading…')
-    
-                            setTimeout(function () {
-                                return menu()
-                            }, 500)
-                        }, 500)
-                    }, 500)
+                    console.log(chalk.red('Wrong password entered!\n'))
+                    let pressenter = await prompts([{ type: 'text', name: 'value', message: 'Press enter to go back to the menu ...'}])
+                    console.clear()
+                    menu()
     
     
                 } else {
                     connection.query(`UPDATE Accounts SET active = '1' WHERE username = '${username.value}'`)
 
                     console.clear()
-                    console.log(chalk.green('Successfully logged in!'))
-                    console.log('↻ | Loading…')
-                    setTimeout(async () => {
-                        console.clear()
-                        console.log(chalk.green('Successfully logged in!'))
-                        console.log('↷ | Loading…')
-    
-                        setTimeout(async () => {
-                            console.clear()
-                            console.log(chalk.green('Successfully logged in!'))
-                            console.log('↻ | Loading…')
-    
-                            setTimeout(async () => {
-                                console.clear()
-                                let choosemenu = await prompts([{ type: 'select', name: 'value', message: 'What do you want to do now?', choices: [{ title: 'Log out', value: 'logout' }, { title: 'Settings', value: 'settings' }, { title: 'Go to work', value: 'work' }], initial: 1 }]);
-                                console.clear()
-                    
-                                eval(choosemenu.value)()
-                            }, 500)
-                        }, 500)
-                    }, 500)
+                    console.log(chalk.green('Successfully logged in!\n'))
+                    let pressenter = await prompts([{ type: 'text', name: 'value', message: 'Press enter to continue ...'}])
+                    console.clear()
+                    let choosemenu = await prompts([{ type: 'select', name: 'value', message: 'What do you want to do now?', choices: [{ title: 'Log out', value: 'logout' }, { title: 'Settings', value: 'settings' }, { title: 'Go to work', value: 'work' }], initial: 1 }]);
+                    console.clear()
+        
+                    eval(choosemenu.value)()
                 }
 
 
