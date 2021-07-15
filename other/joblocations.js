55b566a0180c9d6dee8893571a66bcaf2321d283
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2021-07-15 18:10:05 +0200

    Added other

---
+++ other/joblocations.js
@@ -0,0 +1,32 @@
+exports.LOCATIONS = {
+    "mcmanager": {
+        "locations": [
+            "McDonald's ",
+            "McDrive"
+        ]
+    },
+    "webdev": {
+        "locations": [
+            "Office",
+            "Home"
+        ]
+    },
+    "profgamer": {
+        "locations": [
+            "Home",
+            "Game Group Clubhouse"
+        ]
+    },
+    "youtuber": {
+        "locations": [
+            "Home",
+            "Youtube Studio"
+        ]
+    },
+    "designer": {
+        "locations": [
+            "Office",
+            "Home"
+        ]
+    }
+}
