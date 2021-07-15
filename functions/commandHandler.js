669bf30abe6995df3b38e37a65cca55b0af7a5a1
HyperNaili <72648901+HyperNaili@users.noreply.github.com>
2021-07-15 18:15:54 +0200

    Finished commandHandler.js

---
+++ functions/commandHandler.js
@@ -0,0 +1,23 @@
+ms = require('ms'), fs = require('fs');
+
+module.exports.commandHandler = function commandHandler(commandsmap, aliasesmap) {
+
+    fs.readdirSync('./commands/').forEach(dir => {
+
+        fs.readdir(`./commands/${dir}`, (err, files) => {
+            if (err) console.log(err)
+            
+            var jsFiles = files.filter(f => f.split(".").pop() === "js");
+
+            jsFiles.forEach(file => {
+                var fileGet = require(`../commands/${dir}/${file}`);
+
+                commandsmap.set(fileGet.help.name, fileGet);
+
+                fileGet.help.aliases.forEach(alias => {
+                    aliasesmap.set(alias, fileGet.help.name);
+                })
+            });
+        });
+    });
+}
